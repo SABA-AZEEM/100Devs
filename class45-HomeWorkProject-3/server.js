a7c86cbd018846c66dd75e36d62178e7976f3bdd
Saba Azeem <sabaazeem025@gmail.com>
2023-09-23 15:21:59 +0500

    commit

---
+++ class45-HomeWorkProject-3/server.js
@@ -0,0 +1,80 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import { config } from 'dotenv';
+
+//Load .env variables from .env file
+config();
+//Access environment variables
+const mongodbURI=process.env.MONGODB_URI;
+
+//Create connection with db
+mongoose.connect(mongodbURI);
+//define blog web schema
+const blogWebSchema=new mongoose.Schema({
+    title:{
+        type:String,
+        required:[true,'Please enter title of the post'],
+    },
+    content:{
+        type:String,
+        required:[true,'Please enter post,then submit'],
+    },
+});
+//Create model for blogWebSchema
+const Blog=mongoose.model("Blog",blogWebSchema);
+
+const app=express();
+app.set('view-engine','ejs');
+//use express as body parser setup
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+//set public folder as a static folder
+app.use(express.static('public'));
+const PORT=8000;
+
+
+//root route for all posts
+app.get('/',async(req,res)=>{
+    let blogPosts=await Blog.find();
+    res.render('index.ejs',{
+        posts:blogPosts,
+    });
+});
+//When click read more button then this route work
+app.get('/posts/:id',async(req,res)=>{
+    const id=req.params.id;
+    let post=await Blog.findById(id);
+        res.render('post.ejs',{
+            title:post.title,
+            content:post.content,
+        });
+});
+//if user click on header's compose button
+app.get('/compose',(req,res)=>{
+    res.render('compose.ejs');
+});
+//when publish the post in compose tab
+app.post('/compose',async(req,res)=>{
+    const newPost=new Blog({
+        title:req.body.postTitle,
+        content:req.body.postBody,
+    });
+    try{
+        const result=await newPost.save();
+        res.redirect('/');
+    }catch(error){
+        console.log(error);
+    }
+});
+//route for contact button
+app.get('/contact',(req,res)=>{
+    res.render('contact.ejs');
+});
+//route for about button
+app.get('/about',(req,res)=>{
+    res.render("about.ejs");
+})
+
+app.listen(process.env.PORT || PORT,()=>{
+    console.log(`Server is started on PORT ${PORT}`);
+});
